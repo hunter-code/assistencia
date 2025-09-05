dfbd8eeb6208dc5b0a57ed9ce55526fb9eb48218
Alberdam <alberdam.santiago@gmail.com>
2025-09-05 17:00:05 -0300

    Finalização do front-end inicial, do projeto assistência.

---
+++ js/entrar.js
@@ -0,0 +1,18 @@
+
+
+
+const meuLink  = document.getElementById('showLoginFormLink');
+const meuLink2  = document.getElementById('showLoginFormLink2');
+
+
+  
+   meuLink.addEventListener( 'click', () => {
+    document.getElementById("loginForm").style.display = 'block'; // Mostra o formulário
+    document.getElementById("regiter-Form").style.display = 'block';
+  }
+
+  )
+
+ 
+  document.getElementById('loginForm').style.display = 'block';
+  document.getElementById('regiter-Form').style.display = 'block';
